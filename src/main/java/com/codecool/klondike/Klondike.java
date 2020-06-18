e270217612a1c816969d15325fc6de7ac4ba9191
Gheboianu Bogdan <bogdan.gheboianu.2013@gmail.com>
2020-06-18 22:07:07 +0300

    Added: change background

--- src/main/java/com/codecool/klondike/Klondike.java
+++ src/main/java/com/codecool/klondike/Klondike.java
@@ -2,7 +2,12 @@ package com.codecool.klondike;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Klondike extends Application {
@@ -18,11 +23,56 @@ public class Klondike extends Application {
     public void start(Stage primaryStage) throws InterruptedException {
         Card.loadCardImages();
         Game game = new Game(primaryStage);
-        game.setTableBackground(new Image("/table/green.png"));
+        game.setTableBackground(new Image("/table/red.jpg"));
 
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
+
+        MenuBar menuBar = new MenuBar();
+
+        Menu menuGame = new Menu("Game");
+        MenuItem restart = new MenuItem("Restart");
+        MenuItem exit = new MenuItem("Exit");
+        menuGame.getItems().add(restart);
+        menuGame.getItems().add(exit);
+
+        Menu backgroundMenu = new Menu("Background");
+        MenuItem dark = new MenuItem("Dark");
+        dark.setOnAction(e -> {
+            game.setTableBackground(new Image("/table/dark.jpeg"));
+        });
+        MenuItem light = new MenuItem("Light");
+        light.setOnAction(e -> {
+            game.setTableBackground(new Image("/table/light.jpg"));
+        });
+        MenuItem green = new MenuItem("Green");
+        green.setOnAction(e -> {
+            game.setTableBackground(new Image("/table/green.png"));
+        });
+        MenuItem blue = new MenuItem("Blue");
+        blue.setOnAction(e -> {
+            game.setTableBackground(new Image("/table/blue.jpg"));
+        });
+        MenuItem red = new MenuItem("Red");
+        red.setOnAction(e -> {
+            game.setTableBackground(new Image("/table/red.jpg"));
+        });
+        MenuItem wood = new MenuItem("Wood");
+        wood.setOnAction(e -> {
+            game.setTableBackground(new Image("/table/wood.jpg"));
+        });
+        backgroundMenu.getItems().add(dark);
+        backgroundMenu.getItems().add(light);
+        backgroundMenu.getItems().add(green);
+        backgroundMenu.getItems().add(blue);
+        backgroundMenu.getItems().add(red);
+        backgroundMenu.getItems().add(wood);
+
+        menuBar.getMenus().add(menuGame);
+        menuBar.getMenus().add(backgroundMenu);
+
+        game.getChildren().add(menuBar);
     }
 
 }
